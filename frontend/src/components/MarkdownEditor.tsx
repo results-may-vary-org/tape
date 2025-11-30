0632dc447c3cf22d9c8e391628f1a989b050e794
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-11-30 16:45:49 +0100

    add autocomplete on tag

--- frontend/src/components/MarkdownEditor.tsx
+++ frontend/src/components/MarkdownEditor.tsx
@@ -25,6 +25,42 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = ({content, onChange, fileP
     onChange(data);
   }
 
+  // todo: try to make a wrapper
+  function autoCompleteTag(endTag: string) {
+    if (textareaRef && textareaRef.current) {
+      const textarea = textareaRef.current;
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
+      const text = textarea.value;
+      const selectedText = text.substring(start, end);
+
+      let startTag = "";
+
+      switch (endTag) {
+        case "}":
+          startTag = "{";
+          break;
+        case "]":
+          startTag = "[";
+          break;
+        case ")":
+          startTag = "(";
+          break;
+      }
+
+      // Insert the text at the cursor position
+      textarea.value = text.substring(0, start) + endTag + text.substring(end);
+
+      // Update cursor position
+      textarea.selectionStart = textarea.selectionEnd = start + endTag.length;
+
+      // Focus back on the textarea
+      textarea.focus();
+
+      handleChange(textarea.value);
+    }
+  }
+
   if (!filePath) {
     return (
       <div className="empty-editor">
@@ -38,13 +74,19 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = ({content, onChange, fileP
       <TextArea
         ref={textareaRef}
         value={localContent}
-        placeholder="Write something."
+        placeholder="Write your markdown"
         onChange={(e) => handleChange(e.target.value)}
         style={{
           overflowY: "auto",
-          height: "calc(100vh - (40px + 69px))"
+          height: "calc(100vh - (40px + 69px))", /*the two topbar, this is more quick to do it by hand than using ref*/
         }}
         radius="none"
+        resize="none"
+        onKeyUpCapture={(event) => {
+          if (event.key === "{") autoCompleteTag("}");
+          if (event.key === "[") autoCompleteTag("]");
+          if (event.key === "(") autoCompleteTag(")");
+        }}
       />
     </div>
   );
